b6ef654ea57e3ad7624a4e5d8de77990c2bbd3ce
Marc Chauffour <karmoc@gmail.com>
2012-10-06 18:49:46 +0200

    Creation initiale de l'application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impact::Application.config.secret_token = '59835b2f3f5c6dac513a43d386216375a4b626dd554c4d3f31c3d9694cbb0a1fb0d2da4cb42268cad09295960387fb9ed7ad6ff68cad5194f965932072630d3f'
