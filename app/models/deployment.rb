fe88af1405c9813b00e0f65daa2626e99cd9810b
Marc Chauffour <karmoc@gmail.com>
2012-10-19 07:37:54 +0200

    mise  jour

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -5,4 +5,20 @@ class Deployment < ActiveRecord::Base
   has_many :inverse_deploy_links, :class_name => "DeployLink", :foreign_key => "host_id"
   has_many :connections, :through => :inverse_deploy_links, :source => :deployment  
   attr_accessible :hr, :name, :note
+  
+# comments = Comment.find_by_sql(%Q{
+#     with recursive tree(id) as (
+#         select c.id, c.column1, ...
+#         from comments c
+#         where c.id in (#{roots.join(',')})
+#         union all
+#         select c.id, c.column1, ...
+#         from comments c
+#         join tree on c.parent_id = tree.id
+#     )
+#     select id, column1, ...
+#     from tree
+# })  
+  
+  
 end
